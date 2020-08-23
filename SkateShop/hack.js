d1d5f9de9930f1f2889256056372f67ae0677d3e
Tom <hot@microsoft.com>
2020-08-24 00:12:09 +0800

    save hack.js

--- SkateShop/hack.js
+++ SkateShop/hack.js
@@ -1,6 +1,7 @@
-$(.medium - 7).wrap(function() {
-    var link = $('<a/>');
-    link.attr('href', 'somewhere_far_far_away');
-    link.text($(this).text());
-    return link;
-});
+var node = document.createElement("LI"); // Create a <li> node
+var textnode = document.createTextNode("Water"); // Create a text node
+node.appendChild(textnode); // Append the text to <li>
+document.getElementsByClassName("class=top-bar-section").appendChild(node); // Append <li> to <ul> with id="myList"
+
+
+document.getElementsByClassName("class=top-bar-section").appendChild(node);
