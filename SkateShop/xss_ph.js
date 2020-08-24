2c783fffa6067486ff9b126ecce4780e199dae7e
Tom <hot@microsoft.com>
2020-08-24 12:12:19 +0800

    add ph

--- SkateShop/xss_ph.js
+++ SkateShop/xss_ph.js
@@ -4,9 +4,9 @@ $(".row, .subheader").css("display", "none");
 var place_holder = document.createElement('div');
 place_holder.className = "place_holder";
 
-$(".push").before(place_holder);
+$("nav").after(place_holder);
 $(window).on('load', function() {
-    // Hide after content
+    // Remove all unwanted content
     $(".row").remove();
 
     console.log("done");
