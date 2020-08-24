36630d9a8893169999b1a8a5d9c7f288f85b0092
Tom <hot@microsoft.com>
2020-08-24 12:06:08 +0800

    no display 7

--- SkateShop/xss_nodisplay7.js
+++ SkateShop/xss_nodisplay7.js
@@ -2,12 +2,12 @@
 $(".row, .subheader").css("display", "none");
 
 var place_holder = document.createElement('div');
-div.className = "place_holder";
+place_holder.className = "place_holder";
 
 $(".push").before(place_holder);
 $(window).on('load', function() {
     // Hide after content
-    $(".row, .subheader").css("display", "none");
+    $(".row").remove();
 
     console.log("done");
 });
